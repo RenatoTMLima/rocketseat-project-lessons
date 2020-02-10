7d30487105c0a8fe590beb612a2e152ab7b07d88
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-02-10 14:05:58 -0300

    Finished lesson 10 from chapter 6

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-
+import {Link} from 'react-router-dom';
 import {FaGithubAlt, FaPlus, FaSpinner} from 'react-icons/fa';
 
-import {Container, Form, SubmitButton} from './styles';
+import {Container, Form, SubmitButton, List } from './styles';
 
 import api from '../../services/api';
 
@@ -11,6 +11,22 @@ export default class Main extends Component {
       newRepo: '',
       repositories: [],
       loading: false
+   }
+
+   //carregar os dados do localstorage
+   componentDidMount(){
+      const repositoriesLS = JSON.parse(localStorage.getItem('repositories'));
+
+      if(repositoriesLS) {
+         this.setState({repositories: repositoriesLS});
+      }
+   }
+
+   //salvar os dados do localstorage
+   componentDidUpdate(_, prevState){
+      if(prevState.repositories !== this.state.repositories){
+         localStorage.setItem('repositories', JSON.stringify(this.state.repositories));
+      }
    }
 
    handleInputChange = e => {
@@ -36,6 +52,8 @@ export default class Main extends Component {
    }
    
    render (){
+      const {newRepo, loading, repositories} = this.state;
+
       return(
          <Container>
             <h1>
@@ -47,17 +65,26 @@ export default class Main extends Component {
                <input 
                   type="text"
                   placeholder="Adicionar repositório"
-                  value={this.state.newRepo}
+                  value={newRepo}
                   onChange={this.handleInputChange}
                />
-               <SubmitButton loading={this.state.loading}>
-                  {this.state.loading ? (
+               <SubmitButton loading={loading}>
+                  {loading ? (
                      <FaSpinner color="#FFF" size={14} />
                   ) : (
                      <FaPlus color="#FFF" size={14} />
                   )}
                </SubmitButton>
             </Form>
+
+            <List>
+               {repositories.map(repository => (
+                  <li key={repository.name}>
+                     <span>{repository.name}</span>
+                     <Link to={`/repository/${encodeURIComponent(repository.name)}`}>Detalhes</Link>
+                  </li>
+               ))}
+            </List>
          </Container>
       )
    }
