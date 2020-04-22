6f452fa1f9279e08c933ae3d7f5f45c5eced55b2
Renato Takao Matura de Lima <renato.lima23@fatec.sp.gov.br>
2020-04-21 21:34:19 -0300

    Stopped before the last lesson of the module

--- 05-first-react-project/src/pages/Dashboard/index.tsx
+++ 05-first-react-project/src/pages/Dashboard/index.tsx
@@ -1,44 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-
-import { Title, Form, Repositories } from './styles';
+import { Link } from 'react-router-dom';
+import { Title, Form, Repositories, Error } from './styles';
 
 import logo from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    e: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    e.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      setRepositories([...repositories, response.data]);
+      setNewRepo('');
+      setInputError('');
+    } catch (error) {
+      setInputError('Erro na busca do repositório');
+    }
+  }
+
   return (
     <>
       <img src={logo} alt="Logo" />
       <Title>Explore repositórios no Github</Title>
-      <Form>
-        <input type="text" placeholder="Digite o nome do repositório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
-      <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/49873623?s=460&u=9899fe9d9c921a010e08ad0d8878cacf123920cd&v=4"
-            alt="Takao"
-          />
-          <div>
-            <strong>recketseat/unform</strong>
-            <p>Descrição do repositorio</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+      {inputError && <Error>{inputError}</Error>}
 
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/49873623?s=460&u=9899fe9d9c921a010e08ad0d8878cacf123920cd&v=4"
-            alt="Takao"
-          />
-          <div>
-            <strong>recketseat/unform</strong>
-            <p>Descrição do repositorio</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+      <Repositories>
+        {repositories.map((repository) => (
+          <Link
+            to={`/repository/${repository.full_name}`}
+            key={repository.full_name}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
       </Repositories>
     </>
   );
--- 05-first-react-project/src/pages/Repository/index.tsx
+++ 05-first-react-project/src/pages/Repository/index.tsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Header, RepositoryInfo, Issues } from './styles';
+
+import logo from '../../assets/logo.svg';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-   return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="Foto" />
+          <div>
+            <strong>rocketseat</strong>
+            <p>descrição</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1800</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="ljhh">
+          <div>
+            <strong>khbkhv</strong>
+            <p>khbkhv</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
